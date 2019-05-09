1022bb5f7a0490782d82ae7282423a746de839c7
avbobkovgit <avbobkovgit@mail.ru>
2019-05-10 00:29:47 +0300

    Hello

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 http.createServer(function(request,response){
-     
-    response.end("1231231");
+     21312313
+    response.31231end("1231231");
      
 }).listen(3000, "127.0.0.1",function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
