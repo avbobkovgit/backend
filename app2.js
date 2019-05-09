d3c40b04c207f3aa4001c92e7445af7aea3ca11d
avbobkovgit <avbobkovgit@mail.ru>
2019-05-10 00:58:29 +0300

    Fix files

--- app2.js
+++
@@ -1,8 +0,0 @@
-const modulehttpe = require("http");
-modulehttpe.createServer(function(request,response){
-	response.end("Hello");
-
-	res
-}).listen(3000, "127.0.0.1",function(){
-	console.log("Server is running");
-});
